acdf6fa87926abdad552ff07a554a13a3f68d915
SolowinFXVI <julien.n.jacquet@protonmail.com>
2020-03-03 17:57:19 +0100

    Added UneClasseMetier.java and updated so DIP compliant

---
+++ src/main/java/UneClasseMetier.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UneClasseMetier {
+    final Logger logger = LoggerFactory.getLogger(UneClasseMetier.class);
+
+    public void uneMethodeMetier(){
+        logger.info("Début de uneMethodeMetier");
+        //code
+
+        logger.info("Fin de uneMethodeMetier");
+    }
+}
